d8d56ed0b6dbcfbe99c3bc0735f05251a85426fc
Ric Ian Jamora <48129671+ian1447@users.noreply.github.com>
2024-09-22 22:39:16 +0800

    Family Planning

---
+++ BarangayHealthAid/BarangayHealthAid/ReportForm/FamilyPlanningForm.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace BarangayHealthAid.ReportForm
+{
+    partial class FamilyPlanningForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FamilyPlanningForm));
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.dtFamilyRecords = new DevExpress.XtraGrid.GridControl();
+            this.gvFamilyRecords = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.id = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.name = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemCheckEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.emptySpaceItem2 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.layoutControlGroup2 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.splashScreenManager1 = new DevExpress.XtraSplashScreen.SplashScreenManager(this, typeof(global::BarangayHealthAid.WaitForm1), true, true);
+            this.bwGetFamilyRecords = new System.ComponentModel.BackgroundWorker();
+            this.btnPrint = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.btnRefresh = new DevExpress.XtraEditors.SimpleButton();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            this.layoutControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtFamilyRecords)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvFamilyRecords)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.btnRefresh);
+            this.layoutControl1.Controls.Add(this.btnPrint);
+            this.layoutControl1.Controls.Add(this.dtFamilyRecords);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(647, 429, 250, 347);
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(1273, 605);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // dtFamilyRecords
+            // 
+            this.dtFamilyRecords.Location = new System.Drawing.Point(24, 69);
+            this.dtFamilyRecords.MainView = this.gvFamilyRecords;
+            this.dtFamilyRecords.Name = "dtFamilyRecords";
+            this.dtFamilyRecords.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemCheckEdit1});
+            this.dtFamilyRecords.Size = new System.Drawing.Size(1225, 512);
+            this.dtFamilyRecords.TabIndex = 7;
+            this.dtFamilyRecords.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvFamilyRecords});
+            // 
+            // gvFamilyRecords
+            // 
+            this.gvFamilyRecords.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.id,
+            this.name});
+            this.gvFamilyRecords.GridControl = this.dtFamilyRecords;
+            this.gvFamilyRecords.Name = "gvFamilyRecords";
+            this.gvFamilyRecords.OptionsBehavior.Editable = false;
+            this.gvFamilyRecords.OptionsFind.AlwaysVisible = true;
+            this.gvFamilyRecords.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gvFamilyRecords.OptionsView.RowAutoHeight = true;
+            this.gvFamilyRecords.OptionsView.ShowGroupPanel = false;
+            this.gvFamilyRecords.OptionsView.ShowIndicator = false;
+            // 
+            // id
+            // 
+            this.id.Caption = "id";
+            this.id.FieldName = "id";
+            this.id.Name = "id";
+            // 
+            // name
+            // 
+            this.name.Caption = "Name";
+            this.name.FieldName = "name";
+            this.name.Name = "name";
+            this.name.Visible = true;
+            this.name.VisibleIndex = 0;
+            // 
+            // repositoryItemCheckEdit1
+            // 
+            this.repositoryItemCheckEdit1.AutoHeight = false;
+            this.repositoryItemCheckEdit1.Name = "repositoryItemCheckEdit1";
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.emptySpaceItem2,
+            this.layoutControlGroup2,
+            this.layoutControlItem2,
+            this.layoutControlItem3});
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "Root";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(1273, 605);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // emptySpaceItem2
+            // 
+            this.emptySpaceItem2.AllowHotTrack = false;
+            this.emptySpaceItem2.Location = new System.Drawing.Point(0, 0);
+            this.emptySpaceItem2.Name = "emptySpaceItem2";
+            this.emptySpaceItem2.Size = new System.Drawing.Size(1041, 26);
+            this.emptySpaceItem2.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // layoutControlGroup2
+            // 
+            this.layoutControlGroup2.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1});
+            this.layoutControlGroup2.Location = new System.Drawing.Point(0, 26);
+            this.layoutControlGroup2.Name = "layoutControlGroup2";
+            this.layoutControlGroup2.Size = new System.Drawing.Size(1253, 559);
+            this.layoutControlGroup2.Text = "Records";
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.dtFamilyRecords;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(1229, 516);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // bwGetFamilyRecords
+            // 
+            this.bwGetFamilyRecords.WorkerSupportsCancellation = true;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Image = ((System.Drawing.Image)(resources.GetObject("btnPrint.Image")));
+            this.btnPrint.Location = new System.Drawing.Point(1158, 12);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(103, 22);
+            this.btnPrint.TabIndex = 9;
+            this.btnPrint.Text = "Print";
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.btnPrint;
+            this.layoutControlItem2.Location = new System.Drawing.Point(1146, 0);
+            this.layoutControlItem2.MaxSize = new System.Drawing.Size(107, 26);
+            this.layoutControlItem2.MinSize = new System.Drawing.Size(107, 26);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(107, 26);
+            this.layoutControlItem2.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Image = ((System.Drawing.Image)(resources.GetObject("btnRefresh.Image")));
+            this.btnRefresh.Location = new System.Drawing.Point(1053, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(101, 22);
+            this.btnRefresh.StyleController = this.layoutControl1;
+            this.btnRefresh.TabIndex = 9;
+            this.btnRefresh.Text = "Refresh";
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.btnRefresh;
+            this.layoutControlItem3.Location = new System.Drawing.Point(1041, 0);
+            this.layoutControlItem3.MaxSize = new System.Drawing.Size(105, 26);
+            this.layoutControlItem3.MinSize = new System.Drawing.Size(105, 26);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(105, 26);
+            this.layoutControlItem3.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem3.TextVisible = false;
+            // 
+            // FamilyPlanningForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 605);
+            this.Controls.Add(this.layoutControl1);
+            this.Name = "FamilyPlanningForm";
+            this.Text = "Family Planning";
+            this.Shown += new System.EventHandler(this.FamilyPlanningForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            this.layoutControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtFamilyRecords)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvFamilyRecords)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemCheckEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem2;
+        private DevExpress.XtraGrid.GridControl dtFamilyRecords;
+        private DevExpress.XtraGrid.Views.Grid.GridView gvFamilyRecords;
+        private DevExpress.XtraGrid.Columns.GridColumn id;
+        private DevExpress.XtraEditors.Repository.RepositoryItemCheckEdit repositoryItemCheckEdit1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup2;
+        private DevExpress.XtraGrid.Columns.GridColumn name;
+        private DevExpress.XtraSplashScreen.SplashScreenManager splashScreenManager1;
+        private System.ComponentModel.BackgroundWorker bwGetFamilyRecords;
+        private DevExpress.XtraEditors.SimpleButton btnPrint;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraEditors.SimpleButton btnRefresh;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+    }
+}
---
+++ BarangayHealthAid/BarangayHealthAid/ReportForm/FamilyPlanningForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using BarangayHealthAid.Core;
+using BarangayHealthAid.Dal;
+using BarangayHealthAid.Reports;
+using BarangayHealthAid.ReportForm;
+using DevExpress.XtraReports.UI;
+
+
+namespace BarangayHealthAid.ReportForm
+{
+    public partial class FamilyPlanningForm : DevExpress.XtraEditors.XtraForm
+    {
+        public FamilyPlanningForm()
+        {
+            InitializeComponent();
+        }
+
+        #region UTIL
+        bool loadingIsAlreadyShowing = false;
+        private void ShowLoading(string message)
+        {
+            try
+            {
+                foreach (Control c in this.Controls)
+                {
+                    c.Enabled = false;
+                }
+
+                if (!loadingIsAlreadyShowing)
+                {
+                    loadingIsAlreadyShowing = true;
+                    splashScreenManager1.ShowWaitForm();
+                }
+                splashScreenManager1.SetWaitFormDescription(message);
+            }
+            catch { }
+        }
+
+        private void HideLoading()
+        {
+            try
+            {
+                foreach (Control c in this.Controls)
+                {
+                    c.Enabled = true;
+                }
+
+                loadingIsAlreadyShowing = false;
+                splashScreenManager1.CloseWaitForm();
+            }
+            catch { }
+        }
+
+        #endregion
+
+        private bool SelectionPass()
+        {
+            try
+            {
+                if (!gvFamilyRecords.IsGroupRow(gvFamilyRecords.FocusedRowHandle))
+                {
+                    if (gvFamilyRecords.SelectedRowsCount > 0)
+                    {
+                        return true;
+                    }
+                    else
+                        return false;
+                }
+                else
+                    return false;
+            }
+            catch { return false; }
+        }
+
+        private void FamilyPlanningForm_Shown(object sender, EventArgs e)
+        {
+            layoutControl1.AllowCustomization = false;
+        }
+    }
+}
